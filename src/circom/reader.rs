f724e16e7d63e1dfec82adab9cd2a92fd28baf8e
KitKat676 <diyak@andrew.cmu.edu>
2025-05-21 18:44:15 -0400

    ten inputs sepr

--- src/circom/reader.rs
+++ src/circom/reader.rs
@@ -63,7 +63,7 @@ pub fn generate_witness_from_bin<Fr: PrimeField>(
         witness_output.to_str().expect("Invalid UTF-8 in witness_output path")
     ).expect("Failed to create CString from witness_output");
 
-    println!("Here is the witness input json: {}", witness_input_json);
+    //println!("Here is the witness input json: {}", witness_input_json);
 
     let json_str = CString::new(witness_input_json.as_str())
         .expect("Failed to create CString from witness_input_json");
