8a336bcc49c511c3f826cc74e847550624d7eb1b
KitKat676 <diyak@andrew.cmu.edu>
2025-06-16 17:04:50 -0400

    debugging

--- src/lib.rs
+++ src/lib.rs
@@ -224,7 +224,7 @@ pub fn create_recursive_circuit<G1, G2>(
         };
 
         current_public_output = circuit.get_public_outputs();
-        //println!("The generated public output after iteration {:?} is {:?}", i, current_public_output);
+        println!("The generated public output after iteration {:?} is {:?}", i, current_public_output);
         current_public_input = current_public_output
             .iter()
             .map(|&x| format!("{:?}", x).strip_prefix("0x").unwrap().to_string())
